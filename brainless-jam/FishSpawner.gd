f388021244980b6a72b2ea5b16aa4795e5527243
Thanihan <THANIHANP@GMAIL.com>
2026-07-02 19:53:47 -0400

    Added walls
    -Added invisible walls
    - Changed the spawn point for the fish to be the center

--- brainless-jam/FishSpawner.gd
+++ brainless-jam/FishSpawner.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 @export var fish_scene: PackedScene
-@export var fish_count = 150
+@export var fish_count = 200
 
 func _ready():
+	var screen_center = get_viewport().get_visible_rect().size / 2
+	var world_center = get_canvas_transform().affine_inverse() * screen_center
+
 	for i in fish_count:
 		var fish = fish_scene.instantiate()
-		fish.global_position = global_position + Vector2(randf_range(-150, 150), randf_range(-150, 150))
-		get_tree().current_scene.add_child(fish)
+		fish.global_position = world_center + Vector2(randf_range(-150, 150), randf_range(-150, 150))
+		get_parent().call_deferred("add_child", fish)
+
+	call_deferred("_report_count")
+
+func _report_count():
 	print("Spawned fish count: ", get_tree().get_nodes_in_group("fish").size())
